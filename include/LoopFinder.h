c9153aaad4c158b118521a578ba29f3263536b5b
Петр Жарких <petrzarkih@MacBook-Air-Petr.local>
2026-04-14 20:34:52 +0300

    add new functions and data

--- include/LoopFinder.h
+++ include/LoopFinder.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <set>
 #include <string>
+#include <utility>
 
 class LoopFinder {
 public:
@@ -13,4 +14,12 @@ public:
 
     void addEdge(const std::string& u, const std::string& v);
     void printGraph();
+
+    std::set<std::string> visited;  //не заходим в вершину воторой раз
+    std::set<std::string> inStack;  //вершины на пути рекурсии
+    std::vector<std::pair<std::string, std::string>> backEdges; //список обратных ребер
+
+    void dfs(const std::string& u);
+    void findBackEdges();
+    void printBackEdges() const;
 };
